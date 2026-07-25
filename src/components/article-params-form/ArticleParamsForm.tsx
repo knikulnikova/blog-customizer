9c54b4e9e33894d24fd9c07c6a7aa576e389a3da
Ekaterina Nikulnikova <nikulnikowa2014@gmail.com>
2026-07-25 17:10:53 +0800

    feat: форматирование и открытие формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,28 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	//Состояние открытия-закрытия панели с формой
+	const [isOpen, setisOpen] = useState<boolean>(false);
+
+	const handleToggleSidebar = () => {
+		return setisOpen((prevState) => !prevState);
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleToggleSidebar} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
